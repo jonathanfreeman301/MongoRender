b1396526bb1a29e71a821540730cb28246c9afe9
Jonathan Freeman <123017196+jonathanfreeman301@users.noreply.github.com>
2025-04-08 01:24:25 +0000

    changes

--- testmongo.js
+++ testmongo.js
@@ -33,8 +33,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('jfreeman392');
+    const parts = database.collection('Project 0');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
